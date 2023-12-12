b6275abdc0374dcac16863170c509e98775d0816
Casper Zandbergen <info@casperzandbergen.nl>
2023-12-12 18:52:46 +0100

    Update remap.lua

--- lua/amzd/remap.lua
+++ lua/amzd/remap.lua
@@ -1,10 +1,9 @@
--- editor mappings -
-
 vim.g.mapleader = " "
 
+-- editor mappings -
 vim.keymap.set("v", "<C-c>", "y<Esc>i") -- copy
 vim.keymap.set("v", "<C-x>", "d<Esc>i") -- cut
-vim.keymap.set({"", "i"}, "<C-v>", "<Esc>P=i") -- paste
+vim.keymap.set({"", "i"}, "<C-v>", "<Esc>Pv=i") -- paste
 vim.keymap.set("v", "<C-v>", "\"_d<Esc>P=") -- paste
 vim.keymap.set("i", "<C-z>", "<Esc>ui") -- undo
 vim.keymap.set("", "<C-z>", "<Esc>u") -- undo
@@ -14,13 +13,28 @@ vim.keymap.set("v", "<Backspace>", "s") -- start editing when selecting and usin
 vim.keymap.set("n", "<Backspace>", "s") -- start editing when selecting and using Backspace
 vim.keymap.set("i", "<C-H>", "<Esc>vbd<Esc>i") -- delete until beginning of word (d^ would be to next whitespace) -- C-H == C-Backspace (https://old.reddit.com/r/neovim/comments/okbag3/how_can_i_remap_ctrl_backspace_to_delete_a_word/h5999bi/)
 
+-- correct selection behaviour when in insert mode
+-- - goes into (insert) Visual mode
+-- - selects from in between characters bc thats where the cursor line is
+vim.keymap.set("i", "<S-Left>", "<C-o>h<C-o>v")
+vim.keymap.set("i", "<S-Right>", "<C-o>v")
+
+-- wrap selection in '"<[{(
+vim.keymap.set("v", "'", "s''<Esc>hpa")
+vim.keymap.set("v", '"', 's""<Esc>hpa')
+vim.keymap.set("v", "<", "s<><Esc>hpa")
+vim.keymap.set("v", "[", "s[]<Esc>hpa")
+vim.keymap.set("v", "{", "s{}<Esc>hpa")
+vim.keymap.set("v", "(", "s()<Esc>hpa")
+
+-- movement
 vim.keymap.set("n", "<C-k>", "<cmd>lua vim.diagnostic.goto_next()<CR>zz") -- go to prev error
 vim.keymap.set("n", "<C-j>", "<cmd>lua vim.diagnostic.goto_prev()<CR>zz") -- go to next error
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selected line(s) down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selected line(s) up
 
-vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
-vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>")
+vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>") -- de-stress
+vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>") --
 
 
 -- theprimeagen
@@ -50,6 +64,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/amzd/packer.lua<CR>");
-
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/amzd/packer.lua<CR>")
 
