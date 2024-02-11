68c9adfa5b7416c3cbb7fed6b18e8c18fc4e3d31
Casper Zandbergen <info@casperzandbergen.nl>
2024-02-12 00:24:19 +0100

    swift improvements; use make command for building so errors go to quickfixlist

--- lua/amzd/swift.lua
+++ lua/amzd/swift.lua
@@ -1,16 +1,19 @@
-local swift = vim.api.nvim_create_augroup("swift", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-   pattern = { "swift" },
-   callback = function(event)
-       vim.api.nvim_buf_set_option(event.buf, "commentstring", "// %s")
+    pattern = { "swift" },
+    callback = function(event)
+        vim.api.nvim_buf_set_option(event.buf, "commentstring", "// %s")
+        if vim.fs.find("Package.swift", { path = vim.loop.cwd() }) then
+            vim.api.nvim_buf_set_option(event.buf, "makeprg", "swift")
+        end
 
-       vim.keymap.set("", "<leader>sc", ":!swift package clean; swift package resolve; swift build<cr>")
-       vim.keymap.set("", "<leader>spr", ":!swift package resolve<cr>")
-       vim.keymap.set("", "<leader>st", ":!swift test<cr>")
-       vim.keymap.set("", "<leader>sb", ":!swift build<cr>")
-       vim.keymap.set("", "<leader>sr", ":!swift run<cr>")
-       vim.keymap.set("", "<C-b>", ":!swift build<cr>")
-       vim.keymap.set("", "<C-r>", ":!swift run<cr>")
-   end,
-   group = swift,
+        vim.keymap.set("", "<leader>sc", ":!swift package clean; rm -rf .build; swift package resolve; swift build<cr>")
+        vim.keymap.set("", "<leader>spr", ":!swift package resolve<cr>")
+        vim.keymap.set("", "<leader>st", "<cmd>make test<cr>")
+        vim.keymap.set("", "<leader>sb", "<cmd>make build<cr>")
+        vim.keymap.set("", "<leader>sr", "<cmd>make run<cr>")
+        vim.keymap.set("", "<C-b>", "<cmd>make build<cr>")
+        vim.keymap.set("", "<C-r>", "<cmd>make run<cr>")
+    end,
+    group = vim.api.nvim_create_augroup("swift", { clear = true }),
 })
+
