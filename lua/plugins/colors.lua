18a74b31d1d5620031112ad9ea6f7c8d5ffc9c77
Casper Zandbergen <info@casperzandbergen.nl>
2024-02-11 19:59:00 +0100

    colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,9 @@
 return {
     "arzg/vim-colors-xcode",
-    dependencies = { "nvim-lualine/lualine.nvim" },
+    dependencies = {
+        "nvim-lualine/lualine.nvim",
+        "mawkler/modicator.nvim",
+    },
     config = function ()
         vim.cmd.colorscheme("xcodedarkhc")
 
@@ -19,11 +22,26 @@ return {
         vim.g.VM_Cursor_hl = "Cursor"
         vim.g.VM_Insert_hl = "CursorLine"
 
+        -- Colors taken from all around vim-colors-xcode
+        local custom_onedark = require("lualine.themes.onedark")
+        custom_onedark.normal.a.bg = "#cda1ff"
+        custom_onedark.insert.a.bg = "#4ec4e6"
+        custom_onedark.visual.a.bg = "#ff85b8"
+        custom_onedark.command.a.bg = "#ff8a7a"
+        custom_onedark.replace.a.bg = "#e5cfff"
+        custom_onedark.terminal.a.bg = "#e5cfff"
+
         require("lualine").setup {
             options = {
-                theme = "onedark"
+                theme = custom_onedark
             }
         }
+
+        -- These are required for modicator to work
+        vim.o.cursorline = true
+        vim.o.number = true
+        vim.o.termguicolors = true
+        require('modicator').setup()
 
         -- only one statusline instead of one per window
         vim.opt.laststatus = 3
