541a4043b80f2ddecae733b85abe507bc5e7501b
Casper Zandbergen <info@casperzandbergen.nl>
2024-02-19 17:07:59 +0100

    llm-ls using codellama:13 because it seems fast enough

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -7,7 +7,10 @@ return {
                 "*.*", -- either has file extension
                 "*/zshrc.d/*", -- or in zshrc.d folder
             },
-            lsp = { bin_path = "/home/amzd/.cargo/bin/llm-ls"},
+            lsp = {
+                -- cargo install --rev 16606e5371a1b0582543f03fd8a2666f7bf2580a --git https://github.com/huggingface/llm-ls llm-ls
+                bin_path = "/home/amzd/.cargo/bin/llm-ls",
+            },
             tokens_to_clear = { "<EOT>" },
             fim = {
                 enabled = true,
@@ -18,16 +21,18 @@ return {
             model = "http://localhost:11434/api/generate",
             context_window = 4096,
             tokenizer = {
-                repository = "codellama/CodeLlama-7b-hf",
+                repository = "codellama/CodeLlama-13b-hf",
             },
             adaptor = "ollama",
-            request_body = { model = "codellama:7b-code" },
+            request_body = {
+                model = "codellama:13b-code",
+            },
             query_params = {
                 maxNewTokens = 60,
                 temperature = 0.2,
                 doSample = true,
                 topP = 0.95,
-            }
+            },
         })
     end
 }
