8f14ab55571f666e6c838fd6df0d84d9615807fa
Casper Zandbergen <info@casperzandbergen.nl>
2024-03-04 14:52:54 +0100

    Publish projects run on save

--- lua/amzd/swift.lua
+++ lua/amzd/swift.lua
@@ -17,3 +17,17 @@ vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("swift", { clear = true }),
 })
 
+-- open localhost on save in Publish projects
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = { "*/Content/*.md" },
+    callback = function(event)
+        if vim.fs.find("Package.swift", { path = vim.loop.cwd() })
+            and vim.fs.find(".publish", { path = vim.loop.cwd() }) then
+
+            -- get the name of the only folder inside the Sources directory
+            local folder = vim.fn.glob("Sources/*", 0, 1)[1]:sub(9)
+            vim.fn.jobstart("swift run " .. folder .. " OPEN_LOCALHOST USE_DEBUG_FOLDER SKIP_MARKDOWN_DOWNLOAD")
+        end
+    end,
+    group = vim.api.nvim_create_augroup("swift-publish", { clear = true }),
+})
