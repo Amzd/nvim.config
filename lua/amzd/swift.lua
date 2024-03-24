c73a47ac52f342d7d7a1794fde88c98949566dac
Casper Zandbergen <info@casperzandbergen.nl>
2024-03-24 13:11:59 +0100

    dont force swift as makeprg when there is a Makefile

--- lua/amzd/swift.lua
+++ lua/amzd/swift.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = { "swift" },
     callback = function(event)
         vim.api.nvim_buf_set_option(event.buf, "commentstring", "// %s")
-        if vim.fs.find("Package.swift", { path = vim.loop.cwd() }) then
+        if vim.fs.find("Package.swift", { path = vim.loop.cwd() }) and not vim.fs.find("Makefile", { path = vim.loop.cwd() }) then
             vim.api.nvim_buf_set_option(event.buf, "makeprg", "swift")
         end
 
@@ -11,8 +11,6 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.keymap.set("", "<leader>st", "<cmd>make test<cr>")
         vim.keymap.set("", "<leader>sb", "<cmd>make build<cr>")
         vim.keymap.set("", "<leader>sr", "<cmd>make run<cr>")
-        vim.keymap.set("", "<C-b>", "<cmd>make build<cr>")
-        vim.keymap.set("", "<C-r>", "<cmd>make run<cr>")
     end,
     group = vim.api.nvim_create_augroup("swift", { clear = true }),
 })
